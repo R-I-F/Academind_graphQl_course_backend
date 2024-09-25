0c512a70a964076781dfd0da3d7a599de0fae2b8
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-09-25 12:53:14 +0300

    24.5&6

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const feedRoutes = require('./routes/feed');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.use('/feed', feedRoutes);
 
--- routes/feed.js
+++ routes/feed.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const feedController = require('../controllers/feed')
 
-// GET /feed/posts
+//  GET     /feed/posts
 router.get('/posts', feedController.getPosts);
+
+//  POST    /feed/post
+router.post('/post', feedController.createPost);
 
 module.exports = router ; 
