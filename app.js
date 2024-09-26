51b70416a8b45c00de2b8668df218c7297763bec
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-09-26 13:39:10 +0300

    24.setting up single post route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const feedRoutes = require('./routes/feed');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const multer = require('multer');
+// const multer = require('multer');
 
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,29 +14,36 @@ app.use((req, res, next)=>{
     next();    
 });
 
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, 'images');
-    },
-    filename: (req, file, cb)=>{
-        cb(null, file.originalname);
-    },
-});
+// const fileStorage = multer.diskStorage({
+//     destination: (req, file, cb)=>{
+//         cb(null, 'images');
+//     },
+//     filename: (req, file, cb)=>{
+//         cb(null, file.originalname);
+//     },
+// });
 
-const fileFilter = (req, file, cb)=>{
-    if(file.mimetype === 'image/png' ||file.mimetype === 'image/jpg' ||file.mimetype === 'image/jpeg' ){
-        cb(null, true);
-    } else { cb(null, false); }
-}
+// const fileFilter = (req, file, cb)=>{
+//     if(file.mimetype === 'image/png' ||file.mimetype === 'image/jpg' ||file.mimetype === 'image/jpeg' ){
+//         cb(null, true);
+//     } else { cb(null, false); }
+// }
 
 app.use(bodyParser.json());
 
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter}).single('image'));
+// app.use(multer({ storage: fileStorage, fileFilter: fileFilter}).single('image'));
 
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next)=>{
+    console.log(error);
+    const status = error.statusCode || 500;
+    const mssg = error.message;
+    res.status(status).json({message: message});
+});
 
 mongoose.connect(process.env.DRIVER_URL2)
 .then((result) => { 
--- controllers/feed.js
+++ controllers/feed.js
@@ -2,18 +2,16 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next)=>{
-    res.status(200).json({
-        posts:[
-            {
-                _id: '1',
-                title: 'First Post', 
-                creator: {
-                    name: "Ibrahim Ahmed",
-                },
-                createdAt: new Date(),
-                content: 'This is the first post!'
-            }
-        ]
+    Post.find()
+    .then((posts) => {
+        res.status(200).json({
+            message: 'Post created successfully !',
+            posts: posts
+        })
+    })
+    .catch((err) => {
+        if(!err.statusCode){ err.statusCode = 500; }
+        next(err)
     });
 };
 
@@ -45,4 +43,22 @@ exports.createPost = (req, res, next)=>{
             }
             next(err);
         });
+};
+
+exports.getPost = (req, res, next)=>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then((post) => {
+            if(!post){
+                const error = new Error('post not found !');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({message: 'Post fetched successfully', post: post})
+        })
+        .catch((err) => {
+            if(!err.statusCode){ err.statusCode = 500; }
+            next(err)
+        });
+
 };
