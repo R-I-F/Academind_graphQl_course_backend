6f89fc9790943891b85e618d10ad1377fce9eeca
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-09-26 12:41:35 +0300

    24.setting up static images folder serving and error handling

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const feedRoutes = require('./routes/feed');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-app.use(bodyParser.json());
+const multer = require('multer');
 
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,6 +13,28 @@ app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();    
 });
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'images');
+    },
+    filename: (req, file, cb)=>{
+        cb(null, file.originalname);
+    },
+});
+
+const fileFilter = (req, file, cb)=>{
+    if(file.mimetype === 'image/png' ||file.mimetype === 'image/jpg' ||file.mimetype === 'image/jpeg' ){
+        cb(null, true);
+    } else { cb(null, false); }
+}
+
+app.use(bodyParser.json());
+
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter}).single('image'));
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/feed', feedRoutes);
 
