fb94f1724734341f855f9154ecf84061967207ec
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-09-27 11:31:18 +0300

    24.setting up update Post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
+const fs = require('fs');
+const path = require('path')
 
 exports.getPosts = (req, res, next)=>{
     Post.find()
@@ -28,7 +30,7 @@ exports.createPost = (req, res, next)=>{
     const content = req.body.content;
     const newPost = new Post({
         title: title,
-        imageUrl: image.path,
+        imageUrl: image.path.replace(/\\/g, '/'),
         content: content,
         creator: { name: 'ibrahim ahmed' }
     })
@@ -63,4 +65,54 @@ exports.getPost = (req, res, next)=>{
             next(err)
         });
 
+};
+
+exports.updatePost = (req, res, next)=>{
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const error = new Error('Post validation failed');
+        error.statusCode = 422;
+        throw error;
+    }
+    if(req.file){
+        imageUrl = req.file.path.replace(/\\/g, '/');
+    }
+    if(!imageUrl){
+        const error = new Error('No file picked');
+        error.statusCode = 422;
+        throw error;
+    }
+    Post.findById(postId)
+    .then((post) => {
+        if(!post){
+            const error = new Error('no post found');
+            error.statusCode = 404;
+            throw error
+        }
+        if(imageUrl !== post.imageUrl){
+            clearImage(post.imageUrl);
+        }
+        post.title = title;
+        post.content = content;
+        post.imageUrl = imageUrl;
+        return post.save();
+    })
+    .then((result)=>{
+        res.status(200).json({ message: 'Post updated successfully', post: result });
+    })
+    .catch((err) => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+const clearImage = (filePath)=>{
+    filePath = path.join(__dirname,'../',filePath);
+    fs.unlink(filePath, err => console.log(err));
 };
