f5a3a94128aa8d6b60833cd0b4b5bdf3f5a6a3e8
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-09-30 15:13:26 +0300

    27.websockets: delete post

--- controllers/feed.js
+++ controllers/feed.js
@@ -143,39 +143,37 @@ exports.updatePost = async (req, res, next)=>{
     };
 };
 
-exports.deletePost = (req, res, next)=>{
+exports.deletePost = async (req, res, next)=>{
     const postId = req.params.postId;
-    Post.findById(postId)
-        .then((post) => {
-            if(post.creator.toString() !== req.userId){
-                const error = new Error('Deleting post failed');
-                error.statusCode = 403; // 403 means forbidden
-                throw error ;
-            }
-            if(!post){
-                const error = new Error('Post not found');
-                error.statusCode = 404;
-                throw error;
-            }
-            clearImage(post.imageUrl);
-            return Post.findByIdAndDelete(postId);
-        })
-        .then((result) => {
-            return User.findById(req.userId)
-        })
-        .then((user) => {
-            user.posts.pull(postId)
-            return user.save()
-        })
-        .then((result) => {
-            res.status(200).json({message: "Post deleted successfully."})
-        })
-        .catch((err) => {
-            if(!err.statusCode){
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+    try{
+        const post = await Post.findById(postId)
+
+        if(post.creator.toString() !== req.userId){
+            const error = new Error('Deleting post failed');
+            error.statusCode = 403; // 403 means forbidden
+            throw error ;
+        }
+        if(!post){
+            const error = new Error('Post not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        clearImage(post.imageUrl);
+        const reult = await Post.findByIdAndDelete(postId);
+        const user = await User.findById(req.userId)
+
+        user.posts.pull(postId);
+        const result = await user.save();
+
+        io.getIO().emit('posts', {action: 'delete', post: postId});
+        res.status(200).json({message: "Post deleted successfully."});
+    }
+    catch(err){
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    };
 };
 
 const clearImage = (filePath)=>{
