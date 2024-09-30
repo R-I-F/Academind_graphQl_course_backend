f8756ae030d1bfcc34d6da1c7bd062ad0286ada3
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-09-30 15:00:20 +0300

    27.websockets: mongoose sorting data

--- controllers/feed.js
+++ controllers/feed.js
@@ -14,6 +14,7 @@ exports.getPosts = (req, res, next)=>{
             totalItems = count;
             return Post.find()
                 .populate('creator')
+                .sort({createdAt : -1})
                 .skip((currentPage - 1) * perPage)
                 .limit(perPage)
         })
